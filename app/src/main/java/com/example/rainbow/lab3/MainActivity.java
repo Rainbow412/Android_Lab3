1d97b4e0d3ebb0bf42b4fafe557587cf6cdd6820
Rainbow Liang <Rainbow Liang>
2016-10-09 23:24:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rainbow/lab3/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.rainbow.lab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    String[][] alldata = new String[][]{
+            {"姓名","手机号","类型","归属地","背景颜色"},
+            {"Aaron","17715523654","手机","江苏苏州电信","BB4C3B"},
+            {"Elvis","18825653224","手机","广东揭阳移动","c48d30"},
+            {"David","15052116654","手机","江苏无锡移动","4469b0"},
+            {"Edwin","18854211875","手机","山东青岛移动","20A17B"},
+            {"Frank","13955188541","手机","安徽合肥移动","BB4C3B"},
+            {"Joshua","13621574410","手机","江苏苏州移动","c48d30"},
+            {"Ivan","15684122771","手机","山东烟台联通","4469b0"},
+            {"Mark","17765213579","手机","广东珠海电信","20A17B"},
+            {"Joseph","13315466578","手机","河北石家庄电信","BB4C3B"},
+            {"Phoebe","17895466428","手机","山东东营移动","c48d30"}
+    };
+    List<Map<String, Object>> data = new ArrayList<>();
+    for (int i =1; i < 11; i++){
+        Map<String, Object> temp = new LinkedHashMap<>();
+        for(int j = 0; j < 5; j++)
+            temp.put(alldata[0][j], alldata[i][j]);
+        data.add(temp);
+    }
+
+
+
+}
