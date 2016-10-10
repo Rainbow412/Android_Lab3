6db89981ac03597df067a80079907f181d1d94ef
Rainbow412 <annie0760@163.com>
2016-10-11 02:01:55 +0800

    做好联系人列表 可删除

--- app/src/main/java/com/example/rainbow/lab3/MainActivity.java
+++ app/src/main/java/com/example/rainbow/lab3/MainActivity.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,28 +47,31 @@ public class MainActivity extends AppCompatActivity {
         }
 */
         //data1:首字母、姓名
-        List<Map<String, Object>> data1 = new ArrayList<>();
+        final List<Map<String, Object>> data1 = new ArrayList<>();
         for (int i2 = 1; i2 < 11; i2++) {
             Map<String, Object> temp = new LinkedHashMap<>();
             temp.put("首字母", alldata[i2][0].substring(0,1));
             temp.put("姓名", alldata[i2][0]);
             data1.add(temp);
         }
-        ListView listview = (ListView)findViewById(R.id.contact_list);
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, data1, R.layout.abbr_full,
+        final ListView listview = (ListView)findViewById(R.id.contact_list);
+        final SimpleAdapter simpleAdapter = new SimpleAdapter(this, data1, R.layout.abbr_full,
                 new String[]{"首字母","姓名"}, new int[]{R.id.name_abbr, R.id.name_full});
         listview.setAdapter(simpleAdapter);
 
+
         final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        final Button full_name = (Button)findViewById(R.id.name_full);
-        full_name.setOnLongClickListener(new View.OnLongClickListener() {
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onLongClick(View v) {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                final TextView full_name = (TextView)findViewById(R.id.name_full);
                 alertDialog.setTitle("删除联系人").setMessage("确定删除联系人"+full_name.getText()).setPositiveButton("确定",
                         new DialogInterface.OnClickListener(){
                             @Override
                             public void onClick(DialogInterface dialog, int which){
-                                //删除该联系人
+                                //删除联系人
+                                data1.remove(position);
+                                simpleAdapter.notifyDataSetChanged();
                             }
                         }).setNegativeButton("取消",
                         new DialogInterface.OnClickListener(){
@@ -74,16 +80,13 @@ public class MainActivity extends AppCompatActivity {
                                 //不删除联系人
                             }
                         }).create().show();
+
                 return false;
             }
         });
 
 
 
+
     }
-
-
-
-
-
 }
