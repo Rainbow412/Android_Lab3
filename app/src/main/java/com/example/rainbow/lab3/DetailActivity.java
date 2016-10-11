39213a68e3570bf39b03a6eaae4e788c081b9b24
Rainbow412 <annie0760@163.com>
2016-10-11 21:30:15 +0800

    完成1.0

--- app/src/main/java/com/example/rainbow/lab3/DetailActivity.java
+++ app/src/main/java/com/example/rainbow/lab3/DetailActivity.java
@@ -1,14 +1,29 @@
 package com.example.rainbow.lab3;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 123 on 2016/10/11.
  */
 
 public class DetailActivity extends AppCompatActivity {
+
+    boolean flag = false; //记录星星实心or空心
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +38,53 @@ public class DetailActivity extends AppCompatActivity {
         String where = bundle.getString("归属地", "default value");
         String bg_color = bundle.getString("背景颜色", "default value");
 
+        //设置背景颜色
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.detail_Rlayout);
+        relativeLayout.setBackgroundColor(Color.parseColor(bg_color));
 
+        //设置名字
+        TextView nameText = (TextView)findViewById(R.id.detail_name);
+        nameText.setText(name);
+
+        //设置电话、类型、归属地
+        TextView numText = (TextView)findViewById(R.id.phone_num);
+        numText.setText(phone_num);
+        TextView typeText = (TextView)findViewById(R.id.type);
+        typeText.setText(type);
+        TextView whereText = (TextView)findViewById(R.id.where);
+        whereText.setText(where);
+
+        //operation list赋值
+        final ListView operation_list = (ListView)findViewById(R.id.detail_operation_list);
+        String[] operations = new String[]{"编辑联系人","分享联系人","加入黑名单","删除联系人"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.detail_operation, operations);
+        operation_list.setAdapter(arrayAdapter);
+
+        //点击左上角返回按钮跳转回MainActivity
+        ImageButton back_button = (ImageButton)findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                finish();
+            }
+        });
+
+        //点击切换实心空心星星
+        final ImageButton star_button = (ImageButton)findViewById(R.id.detail_star);
+        star_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!flag){
+                    star_button.setBackgroundResource(R.drawable.full_star);
+                    flag = true;
+                }
+                else{
+                    star_button.setBackgroundResource(R.drawable.empty_star);
+                    flag = false;
+                }
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/rainbow/lab3/MainActivity.java
+++ app/src/main/java/com/example/rainbow/lab3/MainActivity.java
@@ -22,31 +22,31 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     String[][] alldata = new String[][]{
             {"姓名","手机号","类型","归属地","背景颜色"},
-            {"Aaron","17715523654","手机","江苏苏州电信","BB4C3B"},
-            {"Elvis","18825653224","手机","广东揭阳移动","c48d30"},
-            {"David","15052116654","手机","江苏无锡移动","4469b0"},
-            {"Edwin","18854211875","手机","山东青岛移动","20A17B"},
-            {"Frank","13955188541","手机","安徽合肥移动","BB4C3B"},
-            {"Joshua","13621574410","手机","江苏苏州移动","c48d30"},
-            {"Ivan","15684122771","手机","山东烟台联通","4469b0"},
-            {"Mark","17765213579","手机","广东珠海电信","20A17B"},
-            {"Joseph","13315466578","手机","河北石家庄电信","BB4C3B"},
-            {"Phoebe","17895466428","手机","山东东营移动","c48d30"}
+            {"Aaron","17715523654","手机","江苏苏州电信","#BB4C3B"},
+            {"Elvis","18825653224","手机","广东揭阳移动","#c48d30"},
+            {"David","15052116654","手机","江苏无锡移动","#4469b0"},
+            {"Edwin","18854211875","手机","山东青岛移动","#20A17B"},
+            {"Frank","13955188541","手机","安徽合肥移动","#BB4C3B"},
+            {"Joshua","13621574410","手机","江苏苏州移动","#c48d30"},
+            {"Ivan","15684122771","手机","山东烟台联通","#4469b0"},
+            {"Mark","17765213579","手机","广东珠海电信","#20A17B"},
+            {"Joseph","13315466578","手机","河北石家庄电信","#BB4C3B"},
+            {"Phoebe","17895466428","手机","山东东营移动","#c48d30"}
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview_name);
-/*
-        List<Map<String, Object>> data = new ArrayList<>();
+
+        final List<Map<String, Object>> data = new ArrayList<>();
         for (int i1=1; i1<11; i1++) {
             Map<String, Object> temp = new LinkedHashMap<>();
             for(int j = 0; j < 5; j++)
                 temp.put(alldata[0][j], alldata[i1][j]);
             data.add(temp);
         }
-*/
+
         //data1:首字母、姓名
         final List<Map<String, Object>> data1 = new ArrayList<>();
         for (int i2 = 1; i2 < 11; i2++) {
@@ -59,6 +59,24 @@ public class MainActivity extends AppCompatActivity {
         final SimpleAdapter simpleAdapter = new SimpleAdapter(this, data1, R.layout.abbr_full,
                 new String[]{"首字母","姓名"}, new int[]{R.id.name_abbr, R.id.name_full});
         listview.setAdapter(simpleAdapter);
+
+        //短按联系人跳转
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                Bundle bundle = new Bundle();
+
+                bundle.putString("姓名", data.get(position).get("姓名").toString());
+                bundle.putString("手机号", data.get(position).get("手机号").toString());
+                bundle.putString("类型", data.get(position).get("类型").toString());
+                bundle.putString("归属地", data.get(position).get("归属地").toString());
+                bundle.putString("背景颜色", data.get(position).get("背景颜色").toString());
+
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
 
         //长按联系人删除
         final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
@@ -82,26 +100,11 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }).create().show();
 
-                return false;
+                return true;
             }
         });
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-                Bundle bundle = new Bundle();
 
-                bundle.putString("姓名", data1.get(position).get("姓名").toString());
-                bundle.putString("手机号", data1.get(position).get("手机号").toString());
-                bundle.putString("类型", data1.get(position).get("类型").toString());
-                bundle.putString("归属地", data1.get(position).get("归属地").toString());
-                bundle.putString("背景颜色", data1.get(position).get("背景颜色").toString());
-
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
 
     }
 }
